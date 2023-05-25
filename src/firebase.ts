b95534cf65744fd53e73951cf0bd854f2179aef8
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2023-05-25 16:37:10 +0900

    .env 캐시삭제

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAc97iPPZ4Roy628ZNHGkfNs60a9WXxMnY",
+  apiKey:
+    process.env.NODE_ENV === "production"
+      ? process.env.FB_KEY
+      : process.env.NEXT_PUBLIC_FB_KEY,
   authDomain: "iao-pjt.firebaseapp.com",
   projectId: "iao-pjt",
   storageBucket: "iao-pjt.appspot.com",
   messagingSenderId: "899412663684",
-  appId: "1:899412663684:web:0dabe313cb1cfdfcfb91d1",
+  appId: "1:899412663684:web:b4c1c396a73f9accfb91d1",
   measurementId: "G-CESR4142RM",
 };
 
