2f72be13ab6acb8be1a62820f44f026fc4243fcd
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2023-05-31 12:28:41 +0900

    카카오톡 외부 브라우저 로직투가

--- src/pages/guide/[...id].tsx
+++ src/pages/guide/[...id].tsx
@@ -5,6 +5,12 @@ const Index: React.FC = () => {
   const [nav, setNav] = useState<Navigator>();
   const [loc, setLoc] = useState<Location>();
 
+  var target_url =
+    "https://study-webrtc-camera-ic0b1ln19-youdaehoon.vercel.app/guide/1";
+  const x =
+    "kakaotalk://web/openExternal?url=" + encodeURIComponent(target_url);
+  console.log(x);
+
   const route = useRouter();
   const { id } = route.query;
   console.log(id);
@@ -19,6 +25,16 @@ const Index: React.FC = () => {
   }, []);
   return (
     <div>
+      {loc ? (
+        <a
+          href={
+            "kakaotalk://web/openExternal?url=" +
+            encodeURIComponent(location.origin + route.asPath)
+          }
+        >
+          외부링크
+        </a>
+      ) : null}
       {loc ? (
         <input disabled={true} value={location.origin + route.asPath} />
       ) : null}
